016ba62a41b5e5ae44fb5e3f76ce5ca12ffded6d
Melati Eka Putri <melatieka25@gmail.com>
2023-05-18 00:14:47 +0700

    feat: add onboarding logic

---
+++ app/src/main/java/com/calorify/app/ui/activity/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.calorify.app.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.calorify.app.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
