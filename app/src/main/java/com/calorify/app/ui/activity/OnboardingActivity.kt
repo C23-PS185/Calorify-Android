32d763dd00ab4c333ac592157d7ca00c5cc3b498
Melati Eka Putri <melatieka25@gmail.com>
2023-05-22 01:46:00 +0700

    feat: add views in xml

--- app/src/main/java/com/calorify/app/ui/activity/OnboardingActivity.kt
+++ app/src/main/java/com/calorify/app/ui/activity/OnboardingActivity.kt
@@ -1,13 +1,22 @@
 package com.calorify.app.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.calorify.app.R
+import com.calorify.app.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        setContentView(R.layout.activity_onboarding)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            startActivity(Intent(this, AuthActivity::class.java))
+        }
     }
 }
