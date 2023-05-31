23901dbfaa676cd22a1ecbc13922e312e2f438cd
Nivlake <sadiratarunacalvindoro@gmail.com>
2023-06-01 01:49:24 +0700

    Assessment result logic

--- app/src/main/java/com/calorify/app/repository/AssessmentRepository.kt
+++ app/src/main/java/com/calorify/app/repository/AssessmentRepository.kt
@@ -5,6 +5,7 @@ import com.calorify.app.helper.Result
 import androidx.lifecycle.liveData
 import com.calorify.app.data.remote.request.AssessmentRequest
 import com.calorify.app.data.remote.response.AssessmentResponse
+import com.calorify.app.data.remote.response.AssessmentResultResponse
 import com.calorify.app.data.remote.retrofit.ApiService
 
 class AssessmentRepository(private val apiService: ApiService) {
@@ -15,6 +16,25 @@ class AssessmentRepository(private val apiService: ApiService) {
             val response = apiService.uploadAssessment(body)
             if (response.error) {
                 emit(Result.Error(response.message))
+            } else {
+                emit(Result.Success(response))
+            }
+        } catch (e: Exception) {
+            val message = e.message.toString()
+            if (message == "") {
+                emit(Result.Error("Snap, There is something wrong"))
+            } else {
+                emit(Result.Error(message))
+            }
+        }
+    }
+
+    fun getUserResult(userId: String): LiveData<Result<AssessmentResultResponse>> = liveData {
+        emit(Result.Loading)
+        try {
+            val response = apiService.getUserResult(userId)
+            if (response.error) {
+                emit(Result.Error("Data not found"))
             } else {
                 emit(Result.Success(response))
             }
